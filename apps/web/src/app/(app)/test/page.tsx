6472db248792fe9e37fd7ca957674fb8dab94590
Rocky Balboa <rockybalboa@Rockys-Virtual-Machine.local>
2026-02-16 01:45:12 +0400

    test: add minimal /test page to isolate nav issue

---
+++ apps/web/src/app/(app)/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40, color: 'white' }}>
+      <h1>TEST PAGE WORKS</h1>
+      <p>If you can see this, navigation succeeded.</p>
+    </div>
+  );
+}
